16acf293463d72b7346d49c1a8ab8af5d762a0dd
harichandansingh <singh.harichandan@gmail.com>
2017-03-02 17:06:42 -0500

    Made tweaks to constraints for RegisterVC. Button actions pending

--- SplashDash/SplashDash/RegisterViewController.swift
+++ SplashDash/SplashDash/RegisterViewController.swift
@@ -30,10 +30,6 @@ class RegisterViewController: UIViewController {
         self.view.addSubview(usernameTextField)
         self.view.addSubview(passwordTextField)
         self.view.addSubview(detailLabel)
-//        self.view.addSubview(teamColorView1)
-//        self.view.addSubview(teamColorView2)
-//        self.view.addSubview(teamColorView3)
-//        self.view.addSubview(teamColorView4)
         self.view.addSubview(stackview)
         self.view.addSubview(registerButton)
     }
@@ -59,13 +55,15 @@ class RegisterViewController: UIViewController {
         addProfilePicButton.snp.makeConstraints { (view) in
             view.centerX.equalToSuperview()
             view.top.equalTo(avatarImageView.snp.bottom).offset(8.0)
-            view.leading.equalTo(containerView.snp.leading).offset(25.0)
-            view.trailing.equalTo(containerView.snp.trailing).inset(25.0)
+            //view.leading.equalTo(containerView.snp.leading).offset(25.0)
+            //view.trailing.equalTo(containerView.snp.trailing).inset(25.0)
+            view.width.equalTo(avatarImageView.snp.width)
+            view.height.equalTo(20.0)
         }
         
         //usernameTextField
         usernameTextField.snp.makeConstraints { (view) in
-            view.top.equalTo(addProfilePicButton.snp.bottom).offset(16.0)
+            view.top.equalTo(addProfilePicButton.snp.bottom).offset(10.0)
             view.leading.equalTo(containerView.snp.leading).offset(25.0)
             view.trailing.equalTo(containerView.snp.trailing).inset(25.0)
         }
@@ -87,8 +85,15 @@ class RegisterViewController: UIViewController {
         stackview.snp.makeConstraints { (view) in
             view.centerX.equalToSuperview()
             view.top.equalTo(detailLabel.snp.bottom).offset(8.0)
-            view.bottom.equalTo(containerView.snp.bottom).inset(8.0)
+            view.bottom.equalTo(containerView.snp.bottom).inset(16.0)
             view.width.equalTo(avatarImageView.snp.width)
+        }
+        
+        //registerButton
+        registerButton.snp.makeConstraints { (view) in
+            view.centerX.equalToSuperview()
+            view.top.equalTo(containerView.snp.bottom).offset(16.0)
+            view.width.equalTo(containerView.snp.width)
         }
     }
     
@@ -107,7 +112,6 @@ class RegisterViewController: UIViewController {
     lazy var usernameTextField: UITextField = {
         let textField = UITextField()
         textField.placeholder = "Username"
-        textField.font = UIFont.systemFont(ofSize: 12.0)
         textField.autocorrectionType = .no
         textField.backgroundColor = UIColor.white
         textField.borderStyle = .roundedRect
@@ -119,7 +123,6 @@ class RegisterViewController: UIViewController {
     lazy var passwordTextField: UITextField = {
         let textField = UITextField()
         textField.placeholder = "Password"
-        textField.font = UIFont.systemFont(ofSize: 12.0)
         textField.autocorrectionType = .no
         textField.backgroundColor = UIColor.white
         textField.borderStyle = .roundedRect
@@ -132,15 +135,20 @@ class RegisterViewController: UIViewController {
     lazy var avatarImageView: UIImageView = {
         
         //replace this image with a better looking placeholder
-        let image = UIImage(named: "user-icon")
-        let imageView = UIImageView(image: image)
+//        let image = UIImage(named: "user-icon")
+        let imageView = UIImageView()
+        imageView.backgroundColor = UIColor.lightGray
+        imageView.layer.cornerRadius = 10
         imageView.contentMode = .scaleAspectFill
+        imageView.layer.borderColor = UIColor.black.cgColor
+        imageView.layer.borderWidth = 1.0
         
         return imageView
     }()
     
     lazy var detailLabel: UILabel = {
         let label = UILabel()
+        label.font = UIFont.systemFont(ofSize: 12.0)
         label.text = "Select a team:"
         
         return label
@@ -152,8 +160,9 @@ class RegisterViewController: UIViewController {
         //Use color manager to change the backgroundColor to the color determined by Sabrina and design mentor.
         button.backgroundColor = SplashColor.darkPrimaryColor()
         
-        button.layer.cornerRadius = 10
+        button.layer.cornerRadius = 5
         button.setTitle("Add Profile Picture", for: .normal)
+        button.titleLabel?.font = UIFont.systemFont(ofSize: 12.0)
         button.setTitleColor(UIColor.black, for: .normal)
         button.addShadows()
         //button.addTarget(self, action: #selector(), for: .touchUpInside)
@@ -176,58 +185,31 @@ class RegisterViewController: UIViewController {
         
         return button
     }()
-    
-    lazy var teamColorView1: UIView = {
-        let view = UIView()
-        view.backgroundColor = UIColor.red
-        
-        return view
-    }()
-    
-    
-    lazy var teamColorView2: UIView = {
-        let view = UIView()
-        view.backgroundColor = UIColor.black
-        
-        return view
-    }()
-    
-    
-    lazy var teamColorView3: UIView = {
-        let view = UIView()
-        view.backgroundColor = UIColor.green
-        
-        return view
-    }()
-    
-    
-    lazy var teamColorView4: UIView = {
-        let view = UIView()
-        view.backgroundColor = UIColor.orange
-        
-        return view
-    }()
-    
+
     lazy var stackview: UIStackView = {
         let view1 = UIView()
         view1.backgroundColor = UIColor.orange
         view1.layer.cornerRadius = 5
+        view1.addShadows()
         
         let view2 = UIView()
         view2.backgroundColor = UIColor.black
         view2.layer.cornerRadius = 5
+        view2.addShadows()
         
         let view3 = UIView()
         view3.backgroundColor = UIColor.green
         view3.layer.cornerRadius = 5
+        view3.addShadows()
         
         let view4 = UIView()
         view4.backgroundColor = UIColor.red
         view4.layer.cornerRadius = 5
+        view4.addShadows()
         
         let stackview = UIStackView(arrangedSubviews: [view1, view2, view3, view4])
         stackview.axis = .horizontal
-        stackview.spacing = 8.0
+        stackview.spacing = 10.0
         stackview.distribution = .fillEqually
         
         return stackview
