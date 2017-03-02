c529fbc86dc05862c0ef53dcf869a571458afd5e
harichandansingh <singh.harichandan@gmail.com>
2017-03-02 16:05:10 -0500

    RegisterVC version 1 complete constraints set.

--- SplashDash/SplashDash/RegisterViewController.swift
+++ SplashDash/SplashDash/RegisterViewController.swift
@@ -13,10 +13,10 @@ class RegisterViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         setUpViewHierarchy()
-        
+        configureConstraints()
         
         //Use color manager to change the backgroundColor to the color determined by Sabrina and design mentor.
-        self.view.backgroundColor = .white
+        self.view.backgroundColor = SplashColor.primaryColor()
     }
 
     override func didReceiveMemoryWarning() {
@@ -30,11 +30,66 @@ class RegisterViewController: UIViewController {
         self.view.addSubview(usernameTextField)
         self.view.addSubview(passwordTextField)
         self.view.addSubview(detailLabel)
-        self.view.addSubview(teamColorView1)
-        self.view.addSubview(teamColorView2)
-        self.view.addSubview(teamColorView3)
-        self.view.addSubview(teamColorView4)
+//        self.view.addSubview(teamColorView1)
+//        self.view.addSubview(teamColorView2)
+//        self.view.addSubview(teamColorView3)
+//        self.view.addSubview(teamColorView4)
+        self.view.addSubview(stackview)
         self.view.addSubview(registerButton)
+    }
+    
+    func configureConstraints() {
+        //containerView
+        containerView.snp.makeConstraints { (view) in
+            view.centerX.equalToSuperview()
+            view.top.equalTo(self.view.snp.top).offset(130.0)
+            view.height.equalTo(350)
+            view.width.equalTo(250)
+        }
+        
+        //avatarImageView
+        avatarImageView.snp.makeConstraints { (view) in
+            view.centerX.equalToSuperview()
+            view.top.equalTo(containerView.snp.top).offset(8.0)
+            view.width.equalTo(150.0)
+            view.height.equalTo(150.0)
+        }
+        
+        //addProfilePicButton
+        addProfilePicButton.snp.makeConstraints { (view) in
+            view.centerX.equalToSuperview()
+            view.top.equalTo(avatarImageView.snp.bottom).offset(8.0)
+            view.leading.equalTo(containerView.snp.leading).offset(25.0)
+            view.trailing.equalTo(containerView.snp.trailing).inset(25.0)
+        }
+        
+        //usernameTextField
+        usernameTextField.snp.makeConstraints { (view) in
+            view.top.equalTo(addProfilePicButton.snp.bottom).offset(16.0)
+            view.leading.equalTo(containerView.snp.leading).offset(25.0)
+            view.trailing.equalTo(containerView.snp.trailing).inset(25.0)
+        }
+        
+        //passwordTextField
+        passwordTextField.snp.makeConstraints({ (view) in
+            view.top.equalTo(usernameTextField.snp.bottom).offset(16.0)
+            view.leading.equalTo(containerView.snp.leading).offset(25.0)
+            view.trailing.equalTo(containerView.snp.trailing).inset(25.0)
+        })
+        
+        //label
+        detailLabel.snp.makeConstraints { (view) in
+            view.centerX.equalToSuperview()
+            view.top.equalTo(passwordTextField.snp.bottom).offset(8.0)
+        }
+        
+        //stackview
+        stackview.snp.makeConstraints { (view) in
+            view.centerX.equalToSuperview()
+            view.top.equalTo(detailLabel.snp.bottom).offset(8.0)
+            view.bottom.equalTo(containerView.snp.bottom).inset(8.0)
+            view.width.equalTo(avatarImageView.snp.width)
+        }
     }
     
     //MARK: - Lazy Instantiation
@@ -75,6 +130,8 @@ class RegisterViewController: UIViewController {
     }()
     
     lazy var avatarImageView: UIImageView = {
+        
+        //replace this image with a better looking placeholder
         let image = UIImage(named: "user-icon")
         let imageView = UIImageView(image: image)
         imageView.contentMode = .scaleAspectFill
@@ -149,5 +206,30 @@ class RegisterViewController: UIViewController {
         view.backgroundColor = UIColor.orange
         
         return view
+    }()
+    
+    lazy var stackview: UIStackView = {
+        let view1 = UIView()
+        view1.backgroundColor = UIColor.orange
+        view1.layer.cornerRadius = 5
+        
+        let view2 = UIView()
+        view2.backgroundColor = UIColor.black
+        view2.layer.cornerRadius = 5
+        
+        let view3 = UIView()
+        view3.backgroundColor = UIColor.green
+        view3.layer.cornerRadius = 5
+        
+        let view4 = UIView()
+        view4.backgroundColor = UIColor.red
+        view4.layer.cornerRadius = 5
+        
+        let stackview = UIStackView(arrangedSubviews: [view1, view2, view3, view4])
+        stackview.axis = .horizontal
+        stackview.spacing = 8.0
+        stackview.distribution = .fillEqually
+        
+        return stackview
     }()
 }
