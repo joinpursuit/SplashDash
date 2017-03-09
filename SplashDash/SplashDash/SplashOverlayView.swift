1f7bfa9aeab8a37e1c51b5692242e61d21911afb
Tong Lin <tonglin.t91@gmail.com>
2017-03-09 12:44:35 -0500

    move uiimage extension

--- SplashDash/SplashDash/SplashOverlayView.swift
+++ SplashDash/SplashDash/SplashOverlayView.swift
@@ -57,24 +57,3 @@ class SplashOverlayView: MKOverlayRenderer {
         context.draw(imageReference!, in: theRect)
     }
 }
-
-extension UIImage {
-    func imageWithColor(color1: UIColor) -> UIImage {
-        UIGraphicsBeginImageContextWithOptions(self.size, false, self.scale)
-        color1.setFill()
-        
-        let context = UIGraphicsGetCurrentContext()!
-        context.translateBy(x: 0, y: self.size.height)
-        context.scaleBy(x: 1.0, y: -1.0);
-        context.setBlendMode(CGBlendMode.normal)
-        
-        let rect = CGRect(x: 0, y: 0, width: self.size.width, height: self.size.height)
-        context.clip(to: rect, mask: self.cgImage!)
-        context.fill(rect)
-        
-        let newImage = UIGraphicsGetImageFromCurrentImageContext()!
-        UIGraphicsEndImageContext()
-        
-        return newImage
-    }
-}
