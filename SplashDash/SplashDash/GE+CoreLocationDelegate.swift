e0d74ad979ff7924f2b09fed5307b87aa4c65221
Sabrina Ip <kkip90@gmail.com>
2017-03-19 16:30:56 -0400

    splash starts earlier

--- SplashDash/SplashDash/GE+CoreLocationDelegate.swift
+++ SplashDash/SplashDash/GE+CoreLocationDelegate.swift
@@ -30,48 +30,45 @@ extension GameViewController: CLLocationManagerDelegate{
     
     func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
         
-        guard let currentUser = currentUser, self.gameStatus
+        guard let currentUser = currentUser, self.gameStatus, let currentLocation = locations.last
             else { return }
+        
+        
+        //the average human can run at the speed of 15 miles per hour (or 6.7056 meters per second) for short periods of time.
+        
+        // currentSpeed is in meters per second
+        var currentSpeed = currentLocation.speed as Double
+        
+        // range speed for gpx files
+        if currentSpeed == -1 {
+            // setting speed to range between 2.5 m/s to 6 m/s (5.59234 mph to 13.4216mph)
+            currentSpeed = Double(arc4random_uniform(500) + 250)/100
+        }
+        
+        //            print(location.coordinate.latitude)
+        //            print(location.coordinate.longitude)
+        //            print(currentSpeed)
+        //            print(location.timestamp.timeIntervalSince1970)
+        //            print("")
+        
+        let currentUserId = currentUser.uid
+        let coordinate = SplashCoordinate(userID: currentUserId, midCoordinate: currentLocation.coordinate, speed: currentSpeed, teamName: currentUser.teamName, splashImageTag: 1, timestamp: currentLocation.timestamp.timeIntervalSince1970)
+        
+        //push coordinate to firebase
+        self.currentRunCoordinates.append(coordinate)
+        pushSplashToDatabase(coor: coordinate)
+        
         
         if previousLocation == nil {
             previousLocation = locations.first
         } else {
-            if let currentLocation = locations.last {
-                
-                // distance (in meters)
-                var distanceInMeters = previousLocation.distance(from: currentLocation)
-                
-                // error handling
-                if distanceInMeters > 2 * locationManager.distanceFilter {
-                    distanceInMeters = locationManager.distanceFilter
-                }
-                
-                traveledDistanceInMeters += distanceInMeters
-                
-                //the average human can run at the speed of 15 miles per hour (or 6.7056 meters per second) for short periods of time.
-
-                // currentSpeed is in meters per second
-                var currentSpeed = currentLocation.speed as Double
-                
-                // range speed for gpx files
-                if currentSpeed == -1 {
-                    // setting speed to range between 2.5 m/s to 6 m/s (5.59234 mph to 13.4216mph)
-                    currentSpeed = Double(arc4random_uniform(500) + 250)/100
-                }
-                
-                //            print(location.coordinate.latitude)
-                //            print(location.coordinate.longitude)
-                //            print(currentSpeed)
-                //            print(location.timestamp.timeIntervalSince1970)
-                //            print("")
-                
-                let currentUserId = currentUser.uid
-                let coordinate = SplashCoordinate(userID: currentUserId, midCoordinate: currentLocation.coordinate, speed: currentSpeed, teamName: currentUser.teamName, splashImageTag: 1, timestamp: currentLocation.timestamp.timeIntervalSince1970)
-                
-                //push coordinate to firebase
-                self.currentRunCoordinates.append(coordinate)
-                pushSplashToDatabase(coor: coordinate)
+            // distance (in meters)
+            var distanceInMeters = previousLocation.distance(from: currentLocation)
+            // error handling
+            if distanceInMeters > 2 * locationManager.distanceFilter {
+                distanceInMeters = locationManager.distanceFilter
             }
+            traveledDistanceInMeters += distanceInMeters
         }
         // current location becomes the start location
         previousLocation = locations.last
