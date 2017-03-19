15f9486e842a48d0b1a12d46b061270db5017777
Sabrina Ip <kkip90@gmail.com>
2017-03-19 17:12:19 -0400

    take out log in info

--- SplashDash/SplashDash/File.swift
+++ SplashDash/SplashDash/File.swift
@@ -18,8 +18,8 @@ extension HomeViewController{
 //        emailTextField.textField.text = "hari@hari.com"
 //        passwordTextField.textField.text = "123123"
 //        
-        emailTextField.textField.text = "sabrina@gmail.com"
-        passwordTextField.textField.text = "123123"
+//        emailTextField.textField.text = "sabrina@gmail.com"
+//        passwordTextField.textField.text = "123123"
         
     }
 }
